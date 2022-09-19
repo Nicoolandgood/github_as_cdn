22ad92be45de0253b66110eafe9caf7c8b369666
Nicolas Jeuniaux <nicolas.jeuniaux@barestho.com>
2022-09-19 15:56:52 +0200

    mods brought to script

--- my_script.js
+++ my_script.js
@@ -1,1 +1,10 @@
 console.log("My script is running !");
+
+const mycolorshouldbeblue = document.querySelector("p");
+const imbluedabudedabudae = document.querySelector("img");
+
+mycolorshouldbeblue.style.cursor = "pointer";
+
+mycolorshouldbeblue.addEventListener('click', () => {
+    imbluedabudedabudae.style.display = 'block';
+});
